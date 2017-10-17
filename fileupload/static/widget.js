f84d6636656e6d642ed659c35fafa2119a3540af
Marek Dąbrowski <mdabrowski84@gmail.com>
2017-10-17 09:22:34 +0100

    Fix the problem with uploading large files. Added progress update too.

--- fileupload/static/widget.js
+++ fileupload/static/widget.js
@@ -14,6 +14,59 @@ define([
             }
     })();
 
+    var parseFile = function(file, chunkSize, callback, callbackComplete) {
+        var fileSize   = file.size;
+        var offset     = 0;
+        var self       = this; // we need a reference to the current object
+        var chunkReaderBlock = null;
+    
+        var readEventHandler = function(evt) {
+
+            offset += chunkSize;
+
+            if (evt.target.error == null) {
+                console.log("Chunk: ", chunkSize, evt.target.result.length);
+                callback(evt.target.result); // callback for handling read chunk
+            } else {
+                console.log("Read error: " + evt.target.error);
+                return;
+            }
+
+            if (offset >= fileSize) {
+                console.log("Done reading file");
+                callbackComplete();
+                return;
+            }
+    
+            // of to the next chunk
+            window.setTimeout(() => chunkReaderBlock(offset, chunkSize, file), 100);
+        }
+    
+        chunkReaderBlock = function(_offset, length, _file) {
+            var r = new FileReader();
+            var blob = _file.slice(_offset, length + _offset);
+            r.onload = readEventHandler;
+            r.readAsDataURL(blob);
+        }
+    
+        // now let's start the read with the first block
+        chunkReaderBlock(offset, chunkSize, file);
+    };
+
+    var waitfor = function(test, expectedValue, msec, count, source, callback) {
+        // Check if condition met. If not, re-check later (msec).
+        while (test() !== expectedValue) {
+            count++;
+            console.log(source + ': ' + test() + ', expected: ' + expectedValue + ', ' + count + ' loops.');
+            setTimeout(function() {
+                waitfor(test, expectedValue, msec, count, source, callback);
+            }, msec);
+            return;
+        }
+        // Condition finally met. callback() can be executed.
+        window.setTimeout(() => callback(), 150);
+    };
+
     var FileUploadView = widgets.DOMWidgetView.extend({
 
         render: function render () {
@@ -47,14 +100,28 @@ define([
 
             var file = ev.target.files[0];
             var that = this;
-            if (file) {
-                var fileReader = new FileReader();
-                fileReader.onload = function fileReaderOnload () {
+            var chunks = 0;
+            var progress = 0;
+            var chunkSize  = 1000 * 1024; // bytes
+            var totalChunks = Math.ceil(file.size/chunkSize);
 
-                    that.model.set('data_base64', fileReader.result);
-                    that.touch();
-                };
-                fileReader.readAsDataURL(file);
+            if (file) {
+                var a = parseFile(file, chunkSize, (data) => {
+                    console.log("Sending a chunk");
+                    chunks++;
+                    that.model.set('progress', ((chunks/totalChunks)*100).toFixed(0) );
+                    that.model.set('data_chunk', data);
+                    that.touch(); 
+                }, () => {
+                    waitfor( () => {
+                            return that.model.get('count');
+                        }, chunks, 500, 0, '', () => {
+                            console.log("Full data sent");
+                            that.model.set('data_ready', true);
+                            that.touch();
+                        }
+                    )  
+                });
             }
             else {
                 that.send({ event: 'Unable to open file.' });
--- fileupload/widget.py
+++ fileupload/widget.py
@@ -12,14 +12,27 @@ class FileUploadWidget(ipywidgets.DOMWidget):
 
     label = traitlets.Unicode(help='Label on button.').tag(sync=True)
     filename = traitlets.Unicode(help='Filename of `data`.').tag(sync=True)
-    data_base64 = traitlets.Unicode(help='File content, base64 encoded.'
-                                    ).tag(sync=True)
+    
+    data_chunk = traitlets.Unicode(help='File chunk, base64 encoded.').tag(sync=True)
+    data_decoded = traitlets.Bytes(help='File content, partial')
     data = traitlets.Bytes(help='File content.')
+
+    data_ready = traitlets.Bool(help='File uploaded.').tag(sync=True)
+
+    count = traitlets.Int(help='Number of chunks').tag(sync=True)
+    progress = traitlets.Unicode(help='Progress of upload process.').tag(sync=True)
 
     def __init__(self, label="Browse", *args, **kwargs):
         super(FileUploadWidget, self).__init__(*args, **kwargs)
         self._dom_classes += ('widget_item', 'btn-group')
         self.label = label
+        self.count = 0
 
-    def _data_base64_changed(self, *args):
-        self.data = base64.b64decode(self.data_base64.split(',', 1)[1])
+    def _data_chunk_changed(self, *args):
+        self.data_decoded += base64.b64decode(self.data_chunk.split(',', 1)[1])
+        self.count = self.count + 1
+        
+    def _data_ready_changed(self, *args):
+        self.progress = "100"
+        self.data = self.data_decoded
+
